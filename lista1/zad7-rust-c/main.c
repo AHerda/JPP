5408e590fe3d9da7bfd555f4c4feed8df5a8b90a
AHerda <adrianherda@gmail.com>
2024-04-11 23:46:48 +0200

    Done lista1

--- lista1/zad7-rust-c/main.c
+++ lista1/zad7-rust-c/main.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-	printf("Factorial of 5: %llu\n", factorial(5));
+	printf("Factorial of 5: %lu\n", factorial(5));
 	printf("\tCorrect: 120\n");
 
-	printf("GCD of 15 and 25: %llu\n", gcd(15, 25));
+	printf("GCD of 15 and 25: %lu\n", gcd(15, 25));
 	printf("\tCorrect: 5\n");
 
 	Solution solution = diophantine(3, 7, 22);
-	printf("Diophantine solution for 3x + 7y = 22: x = %lld, y = %lld\n", solution.x, solution.y);
+	printf("Diophantine solution for 3x + 7y = 22: x = %ld, y = %ld\n", solution.x, solution.y);
 	printf("\tCorrect: x = 5, y = 1\n");
 	printf("\tOther correct: x = -44, y = 22\n");
 	return 0;
